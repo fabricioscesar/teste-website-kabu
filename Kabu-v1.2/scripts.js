ae3bf8b8a95c253dd67a08d1aa41c57fddc88e39
fabricioscesar <90213175+fabricioscesar@users.noreply.github.com>
2024-01-04 20:54:24 -0300

    upload kabu v1.2
    upload da primeira versão finalizada para testes

---
+++ Kabu-v1.2/scripts.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let currentIndex = 0;
+
+    function changeSlide(direction) {
+        const slides = document.querySelectorAll('.carousel-slide');
+        currentIndex = (currentIndex + direction + slides.length) % slides.length;
+        const translateValue = -100 * currentIndex + '%';
+        document.querySelector('.carousel-container').style.transform = 'translateX(' + translateValue + ')';
+    }
+
+    document.querySelector('.next-button').addEventListener('click', function () {
+        changeSlide(1);
+    });
+
+    document.querySelector('.prev-button').addEventListener('click', function () {
+        changeSlide(-1);
+    });
+
+    setInterval(function () {
+        changeSlide(1);
+    }, 10000); // Troca de slide a cada 5 segundos (ajuste conforme necessário)
+});
